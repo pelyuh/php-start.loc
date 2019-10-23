297730434e82a066e1f3107652a42bcb6311a1f6
Pelyukh Vitaliy <pelyuh.v@gmail.com>
2019-10-23 10:52:50 +0300

    Task 1, 2, 3

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+echo 'Lesson 2: The Basics';
+
+echo '<br><br><hr><br>';
+
+
+// BEGIN: Task 1. Comments
+
+echo 'Task 1. Comments' . '<br><br>';
+
+/*
+ * 23 October 2019 year
+ */
+
+// Vitaliy
+
+echo 'Hello World!';
+
+// END: Task 1. Comments
+
+
+echo '<br><br><hr><br>';
+
+
+// BEGIN: Task 2. Variable naming
+
+echo 'Task 2. Variable naming' . '<br><br>';
+
+$channelName = 'CNN';
+
+$manufacturerAddress = 'Ukraine, Kiev, st. Kovelskaya, 2';
+
+$carColor = 'blue';
+
+$waterTemperature = '36 C';
+
+$phoneModel = 'Meizu M5 Note';
+
+echo '$channelName<br>';
+echo '$manufacturerAddress<br>';
+echo '$carColor<br>';
+echo '$waterTemperature<br>';
+echo '$phoneModel<br>';
+
+// END: Task 2. Variable naming
+
+
+echo '<br><br><hr><br>';
+
+
+// BEGIN: Task 3: Operators
+
+echo 'Task 3: Operators' . '<br><br>';
+
+$one = 3;
+$two = 5;
+$three = 8;
+
+$count = $one + $two + $three;
+
+echo $count . '<br>';
+
+echo $result = 2+6+2/5-1 . '<br>';
+
+// END: Task 3: Operators
+
+
+echo '<br><br><hr><br>';
